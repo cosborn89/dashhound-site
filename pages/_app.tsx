89af51f1d7c4d8c76c19bd3542de14a5874cd739
cosborn89 <carlanthonyosborn@gmail.com>
2025-04-15 21:44:11 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import builder from '@builder.io/react'
 
-// Initialize Builder.io with your public API key
 builder.init('a10a5490d2084e809b7c91b58101858c')
 
 export default function App({ Component, pageProps }: AppProps) {
